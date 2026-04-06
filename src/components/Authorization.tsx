4cf62d87a87fe07e5e972ede1364262f352e36da
Safronov <Safronovmaxiim@mail.ru>
2026-04-06 15:56:24 +0300

    Add toggle button for password,  little style for btn showPassword, moved to separate fail - url

--- src/components/Authorization.tsx
+++ src/components/Authorization.tsx
@@ -2,16 +2,16 @@ import { useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import { useState } from 'react';
 import styles from './authorization.module.scss';
-import photo from '../assets/hidepassword.svg';
+import { url } from '../api/url';
+
 interface UserLogin {
   name: string;
   password: string;
 }
 
-const URL = `http://localhost:3000`;
-
 function Authorization() {
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
@@ -26,9 +26,11 @@ function Authorization() {
     },
   });
 
+  const togglePassword = () => setShowPassword((prev) => !prev);
+
   const onSubmit = async (data: UserLogin) => {
     try {
-      const response = await fetch(`${URL}/login`, {
+      const response = await fetch(`${url}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: data.name, password: data.password }),
@@ -43,7 +45,6 @@ function Authorization() {
       reset();
       setError('');
     } catch (error) {
-      console.error('Ошибка при логине:', error);
       setError(error.message);
     }
   };
@@ -57,7 +58,7 @@ function Authorization() {
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="name">
           <label htmlFor="name">
-            name
+            <span>name</span>
             <input
               style={{ display: 'relative' }}
               id="name"
@@ -75,16 +76,25 @@ function Authorization() {
 
         <div className="password">
           <label htmlFor="password">
-            password
+            <span>password</span>
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               id="password"
               {...register('password', {
                 required: true,
               })}
             />
+          </label>
+          <button
+            type="button"
+            className={
+              showPassword
+                ? `${styles.passwordIconActive}`
+                : `${styles.passwordIcon}`
+            }
+            onClick={togglePassword}
+          >
             <svg
-              className={`${styles.passwordIcon}`}
               width="18"
               height="16"
               viewBox="0 0 18 16"
@@ -94,40 +104,40 @@ function Authorization() {
               <path
                 d="M4.34984 12.7748C2.4915 11.5582 1.2915 9.72482 1.2915 8.11649C1.2915 5.38316 4.7415 2.03316 8.99984 2.03316C10.7415 2.03316 12.3582 2.59149 13.6582 3.45816"
                 stroke=""
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M15.5414 5.17517C16.2839 6.11684 16.7164 7.1585 16.7164 8.11684C16.7164 10.8502 13.2581 14.2002 8.99977 14.2002C8.24144 14.2002 7.50061 14.0918 6.80811 13.9002"
                 stroke=""
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M7.13804 9.97249C6.6422 9.48166 6.3647 8.81249 6.3672 8.11499C6.36387 6.66083 7.54054 5.47916 8.99554 5.47666C9.69554 5.47499 10.3672 5.75249 10.8622 6.24749"
                 stroke=""
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M11.5912 8.58258C11.3962 9.65925 10.5537 10.5034 9.47705 10.7009"
                 stroke=""
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M15.5762 1.54158L2.43115 14.6866"
                 stroke=""
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
-          </label>
+          </button>
         </div>
         <button type="submit">Войти</button>
       </form>
